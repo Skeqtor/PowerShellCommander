47fe628d98f311c4f2a3bd99ef630ff320e99ec8
- <->
2022-01-04 19:40:13 +0700

    Add project files.

---
+++ PowerShellCommander/Form1.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtCommand = New System.Windows.Forms.TextBox()
+        Me.btnRun = New System.Windows.Forms.Button()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 417)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Command:"
+        '
+        'txtCommand
+        '
+        Me.txtCommand.Location = New System.Drawing.Point(72, 414)
+        Me.txtCommand.Name = "txtCommand"
+        Me.txtCommand.Size = New System.Drawing.Size(635, 20)
+        Me.txtCommand.TabIndex = 1
+        '
+        'btnRun
+        '
+        Me.btnRun.Location = New System.Drawing.Point(713, 412)
+        Me.btnRun.Name = "btnRun"
+        Me.btnRun.Size = New System.Drawing.Size(75, 23)
+        Me.btnRun.TabIndex = 2
+        Me.btnRun.Text = "Run Script"
+        Me.btnRun.UseVisualStyleBackColor = True
+        '
+        'txtOutput
+        '
+        Me.txtOutput.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.txtOutput.Font = New System.Drawing.Font("Lucida Console", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.txtOutput.ForeColor = System.Drawing.Color.White
+        Me.txtOutput.Location = New System.Drawing.Point(13, 13)
+        Me.txtOutput.Multiline = True
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.Size = New System.Drawing.Size(775, 393)
+        Me.txtOutput.TabIndex = 3
+        Me.txtOutput.WordWrap = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.btnRun)
+        Me.Controls.Add(Me.txtCommand)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "PowerShell Commander"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtCommand As TextBox
+    Friend WithEvents btnRun As Button
+    Friend WithEvents txtOutput As TextBox
+End Class
---
+++ PowerShellCommander/Form1.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Collections.ObjectModel
+Imports System.Management.Automation
+Imports System.Management.Automation.Runspaces
+Imports System.Text
+
+Public Class Form1
+    Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles btnRun.Click
+        txtOutput.Clear()
+        txtOutput.Text = RunScript(txtCommand.Text)
+    End Sub
+
+    Private Function RunScript(ByVal script As String) As String
+        Dim runspace As Runspace = RunspaceFactory.CreateRunspace()
+        runspace.Open()
+        Dim pipeline As Pipeline = runspace.CreatePipeline()
+        pipeline.Commands.AddScript(script)
+        pipeline.Commands.Add("Out-String")
+        Dim result As Collection(Of PSObject) = pipeline.Invoke()
+        runspace.Close()
+        Dim stringBuilder As StringBuilder = New StringBuilder()
+        For Each ps As PSObject In result
+            stringBuilder.AppendLine(ps.ToString())
+
+        Next
+        Return stringBuilder.ToString()
+    End Function
+End Class
